bbf21b51094d293c0cb8d320b628670380f9aaa3
Aj Castiglione <antoniocastiglione10@gmail.com>
2018-05-06 12:21:07 -0400

    almost finished home pg. just have to finish the footer section then its complete for design aspect

--- page-home.php
+++ page-home.php
@@ -6,60 +6,85 @@
 
 	<?php get_header(); ?>
 
-	<section class="banner-section cf">
+	<div id="content">
 
-		<?php
+		<main id="main" class="cf col-xs-12" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+
+			<section class="banner-section cf">
+				<?php
 		$images = get_field('slider_images');
 		
 		if($images) : ?>
-			<div id="slider" class="slick-slider-container">
-				<ul class="slider-container">
-					<?php foreach( $images as $image ): ?>
-					<li>
-						<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-					</li>
-					<?php endforeach; ?>
-				</ul>
-			</div>
-			<?php endif; ?>
+					<div id="slider" class="slick-slider-container">
+						<ul class="slider-container">
+							<?php foreach( $images as $image ): ?>
+							<li>
+								<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+							</li>
+							<?php endforeach; ?>
+						</ul>
+					</div>
+					<?php endif; ?>
+			</section>
 
-	</section>
+			<section class="about-us-section">
+				<div class="inner-about-us wrap cf">
+					<h1 class="about-us-title">Your title goes here</h1>
+					<p class="about-us-content">
+						<strong>Classic Events</strong> boast WNY's premier banquet properties
+						<br> known for the picturesque ambience, fine food and first class service.
+						<br> Each property is unique and offers an array of catering options - the perfect setting for any occasion.
+					</p>
+				</div>
+			</section>
 
-	<section class="about-us-section">
-		<div class="inner-about-us wrap cf">
-			<h1 class="about-us-title">Your title goes here</h1>
-			<p class="about-us-content"><strong>Classic Events</strong> boast WNY's premier banquet properties known for the picturesque ambience, fine food and first class service. Each property is unique and offers an array of catering options - the perfect setting for any occasion.</p>
-		</div>
-	</section>
+			<section class="video-section">
+				{Video will go here once its uploaded}
+			</section>
 
-	<div id="content">
+			<section class="venue-section">
+				<div class="inner-venue large-wrapper">
+					<h1 class="venue-title">
+						<span>Venues</span>
+					</h1>
 
-		<div id="inner-content" class="wrap cf">
+					<div class="venue-boxes">
+						<?php while( have_rows('venues') ) : the_row() ?>
+						<div class="venue-item">
+							<a href="<?php echo get_sub_field('target_url'); ?>">
+								<div class="venue-image">
+									<?php $img = get_sub_field('image');
+								if(!empty($img)) : ?>
+									<img src="<?php echo $img['url']; ?>" alt="<?php echo $img['alt']; ?>" />
+									<?php endif; ?>
+								</div>
+								<div class="venue-single-title">
+									<h2>
+										<?php echo get_sub_field('title'); ?>
+									</h2>
+								</div>
+							</a>
+						</div>
+						<?php endwhile; ?>
+					</div>
+				</div>
+			</section>
 
-			<main id="main" class="cf col-xs-12" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+			<section class="pop-up-restaurants">
+				<div class="large-wrapper">
+					<h2 class="pop-up-title">pop-up restaurants</h2>
+				</div>
 
-				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<div class="inner-pop-up wrap cf">
+					<img src="http://localhost/classic-events/wp-content/uploads/2018/05/pop-up-logo.png">
+					<a href="#" class="sign-up-btn">Sign up to be notified <br> when events pop up!</a>
+				</div>
+			</section>
 
-				<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+		</main>
 
-					<header class="article-header">
-
-						<h1 class="page-title">
-							<?php the_title(); ?>
-						</h1>
-
-					</header>
-
-					<section class="entry-content cf" itemprop="articleBody">
-						<?php the_content(); ?>
-					</section>
-
-				</article>
-
-				<?php endwhile; endif; ?>
-
-			</main>
-		</div>
+	</div>
+	<!--#content-->
 
 	</div>
 
