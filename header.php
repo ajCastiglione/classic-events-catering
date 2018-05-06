391143f93733a77c61880caf390117bea25e4f79
Aj Castiglione <antoniocastiglione10@gmail.com>
2018-05-06 11:13:27 -0400

    finished the header. implemented slider. working on rest of home page. total time so far is 1 hour

--- header.php
+++ header.php
@@ -3,56 +3,65 @@
 <!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
 <!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
 <!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
-<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
+<!--[if gt IE 8]><!-->
+<html <?php language_attributes(); ?> class="no-js">
+<!--<![endif]-->
 
-	<head>
-		<meta charset="utf-8">
+<head>
+	<meta charset="utf-8">
 
-		<?php // force Internet Explorer to use the latest rendering engine available ?>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<?php // force Internet Explorer to use the latest rendering engine available ?>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-		<title><?php wp_title(''); ?></title>
+	<title>
+		<?php wp_title(''); ?>
+	</title>
 
-		<?php // mobile meta (hooray!) ?>
-		<meta name="HandheldFriendly" content="True">
-		<meta name="MobileOptimized" content="320">
-		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+	<?php // mobile meta (hooray!) ?>
+	<meta name="HandheldFriendly" content="True">
+	<meta name="MobileOptimized" content="320">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
 
-		<?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
-		<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-touch-icon.png">
-		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
-		<!--[if IE]>
+	<?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
+	<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-touch-icon.png">
+	<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+	<!--[if IE]>
 			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
 		<![endif]-->
-		<?php // or, set /favicon.ico for IE10 win ?>
-		<meta name="msapplication-TileColor" content="#f01d4f">
-		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
-            <meta name="theme-color" content="#121212">
+	<?php // or, set /favicon.ico for IE10 win ?>
+	<meta name="msapplication-TileColor" content="#f01d4f">
+	<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
+	<meta name="theme-color" content="#121212">
 
-		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
-		<!--external stylesheets / fonts / etc...-->
+	<!--external stylesheets / fonts / etc...-->
+	<link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700,700i" rel="stylesheet">
 
-		<?php // wordpress head functions ?>
-		<?php wp_head(); ?>
-		<?php // end of wordpress head ?>
+	<?php // wordpress head functions ?>
+	<?php wp_head(); ?>
+	<?php // end of wordpress head ?>
 
-		<?php // drop Google Analytics Here ?>
-		<?php // end analytics ?>
+	<?php // drop Google Analytics Here ?>
+	<?php // end analytics ?>
 
-	</head>
+</head>
 
-	<body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
+<body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
 
-		<div id="container">
+	<div id="container">
 
-			<header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
+		<header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
 
-				<div id="inner-header" class="wrap cf">
+			<div id="inner-header" class="cf">
 
-					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-					<p id="logo" class="h1" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+				<div class="logo-container">
+					<a href="<?php echo home_url(); ?>" rel="nofollow">
+						<img src="<?php echo get_template_directory_uri();?>/library/images/logo.png" id="logo" class="h1" itemscope itemtype="http://schema.org/Organization">
+					</a>
+				</div>
 
+				<div class="nav-container">
 					<nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
 						<?php wp_nav_menu(array(
     					         'container' => false,                           // remove nav container
@@ -68,21 +77,22 @@
     					         'fallback_cb' => 'bones_main_nav_fallback'                             // fallback function (if there is one)
 						)); ?>
 
-<?php // this is the fallback for header menu
-function bones_main_nav_fallback() {
-	wp_page_menu( array(
-		'show_home' => true,
-    	'menu_class' => 'nav top-nav cf',      // adding custom nav class
-		'include'     => '',
-		'exclude'     => '',
-		'echo'        => true,
-        'link_before' => '',                            // before each link
-        'link_after' => ''                             // after each link
-	) );
-}
-?>					
+						<?php // this is the fallback for header menu
+						function bones_main_nav_fallback() {
+							wp_page_menu( array(
+								'show_home' => true,
+								'menu_class' => 'nav top-nav cf',      // adding custom nav class
+								'include'     => '',
+								'exclude'     => '',
+								'echo'        => true,
+								'link_before' => '',                            // before each link
+								'link_after' => ''                             // after each link
+							) );
+						}
+					?>
 					</nav>
-
 				</div>
 
-			</header>
+			</div>
+
+		</header>
--- page-home.php
+++ page-home.php
@@ -4,38 +4,64 @@
 */
 ?>
 
-<?php get_header(); ?>
+	<?php get_header(); ?>
 
-			<div id="content">
+	<section class="banner-section cf">
 
-				<div id="inner-content" class="wrap cf">
-
-						<main id="main" class="cf col-xs-12" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
-
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
-
-								<header class="article-header">
-
-									<h1 class="page-title"><?php the_title(); ?></h1>
-
-								</header>
-
-								<section class="entry-content cf" itemprop="articleBody">
-									<?php the_content(); ?>
-								</section>
-
-							</article>
-
-							<?php endwhile; endif; ?>
-
-							<?php  ?>
-
-						</main>
-				</div>
-
+		<?php
+		$images = get_field('slider_images');
+		
+		if($images) : ?>
+			<div id="slider" class="slick-slider-container">
+				<ul class="slider-container">
+					<?php foreach( $images as $image ): ?>
+					<li>
+						<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+					</li>
+					<?php endforeach; ?>
+				</ul>
 			</div>
+			<?php endif; ?>
+
+	</section>
+
+	<section class="about-us-section">
+		<div class="inner-about-us wrap cf">
+			<h1 class="about-us-title">Your title goes here</h1>
+			<p class="about-us-content"><strong>Classic Events</strong> boast WNY's premier banquet properties known for the picturesque ambience, fine food and first class service. Each property is unique and offers an array of catering options - the perfect setting for any occasion.</p>
+		</div>
+	</section>
+
+	<div id="content">
+
+		<div id="inner-content" class="wrap cf">
+
+			<main id="main" class="cf col-xs-12" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+				<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+
+					<header class="article-header">
+
+						<h1 class="page-title">
+							<?php the_title(); ?>
+						</h1>
+
+					</header>
+
+					<section class="entry-content cf" itemprop="articleBody">
+						<?php the_content(); ?>
+					</section>
+
+				</article>
+
+				<?php endwhile; endif; ?>
+
+			</main>
+		</div>
+
+	</div>
 
 
-<?php get_footer(); ?>
+	<?php get_footer(); ?>
